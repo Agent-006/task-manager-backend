364fc8c3ac5a91df593c12bbe291bcaa0b4a1255
Sagar~ <sagargope2001@gmail.com>
2024-03-12 01:48:55 +0530

    add user and task models

---
+++ src/models/task.models.js
@@ -0,0 +1,25 @@
+import mongoose, { Schema } from "mongoose";
+
+const taskSchema = new Schema(
+    {
+        title: {
+            type: String,
+            required: true,
+        },
+        description: {
+            type: String,
+            required: true,
+        },
+        content: {
+            type: String,
+            required: true,
+        },
+        owner: {
+            type: Schema.Types.ObjectId,
+            ref: "User",
+        },
+    },
+    { timestamps: true }
+);
+
+export const Task = mongoose.model("Task", taskSchema);
